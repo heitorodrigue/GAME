19667e99a69e14c5361ad5415c6226c56af46f27
heitorodrigue <160082563+heitorodrigue@users.noreply.github.com>
2026-08-17 09:44:33 -0300

    Add files via upload

---
+++ Prototype_01/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections.Specialized;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float turnSpeed = 100f;
+    public InputAction MoveAction;
+    private Vector2 moveInput;
+    void Start()
+    {
+        MoveAction.Enable();
+    }
+    
+    void Update()
+    {
+        moveInput = MoveAction.ReadValue<Vector2>();
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * moveInput.y);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * moveInput.x);
+    }
+}
